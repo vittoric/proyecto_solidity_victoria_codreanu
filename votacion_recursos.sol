4e1c1fd6e5e1b2b57b98b3393d1e2fb9ac7282e1
toricode <93945847+vittoric@users.noreply.github.com>
2023-02-07 23:36:54 +0100

    Create votacion_recursos.sol

---
+++ votacion_recursos.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.8.0;
+
+contract Voting {
+    // Estructura para almacenar los recursos
+    struct Resource {
+        string name;
+        uint id;
+        uint votes;
+    }
+    
+    // Arreglo de recursos
+    Resource[] public resources;
+    
+    // Diccionario para almacenar los votantes autorizados
+    mapping(address => bool) public authorizedVoters;
+    
+    // Tiempo límite de la votación
+    uint public votingDeadline;
+    
+    // Propietario del contrato
+    address public owner;
+    
+    // Constructor
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    // Función para autorizar a los votantes
+    function authorizeVoter(address voter) public {
+        require(msg.sender == owner, "Sólo el dueño del contrato puede autorizar a los votantes");
+        authorizedVoters[voter] = true;
+    }
+    
+    // Función para registrar los recursos
+    function registerResource(string memory name, uint id) public {
+        require(msg.sender == owner, "Sólo el dueño del contrato puede registrar recursos");
+        resources.push(Resource(name, id, 0));
+    }
+    
+    // Función para votar por un recurso
+    function vote(uint resourceId) public {
+        require(authorizedVoters[msg.sender], "El votante no está autorizado");
+        require(now <= votingDeadline, "La votación ha finalizado");
+        
+        // Verificar si el recurso existe
+        uint resourceIndex;
+        for (uint i = 0; i < resources.length; i++) {
+            if (resources[i].id == resourceId) {
+                resourceIndex = i;
+                break;
+            }
+        }
+        require(resourceIndex < resources.length, "El recurso no existe");
+        
+        // Aumentar el número de votos para el recurso
+        resources[resourceIndex].votes++;
+    }
+    
+    // Función para obtener el recurso ganador
+    function getWinner() public view returns (uint winningResourceId) {
+        uint maxVotes = 0;
+        for (uint i = 0; i < resources.length; i++) {
+            if (resources[i].votes > maxVotes) {
+                maxVotes = resources[i].votes;
+                winningResourceId = resources[i].id;
+            }
+        }
+    }
+}
